7c1093f0a0327fd305e6d98f8d6008ff08a216c6
DaMrZ <daniel.bemrose@hotmail.com>
2022-03-13 17:13:28 +0000

    simple

--- startup.lua
+++ startup.lua
@@ -1,7 +1,1 @@
-if fs.exists("bigfont") == false then
-    pastebin get 3LfWxRWh bigfont
-elseif fs.exists("LobbyMonitor.lua") == false then
-    wget https://raw.githubusercontent.com/DaMrZ/LobbyMonitors/main/LobbyMonitors.lua LobbyMonitors.lua
-else
-    shell.run("LobbyMonitors.lua")
-end
+shell.run("LobbyMonitors.lua")
